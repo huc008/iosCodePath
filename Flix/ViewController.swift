b2d9bf3e64501cf978485664ccb3a083dfad0a11
huc008 <chenhy997@gmail.com>
2022-02-27 21:42:43 -0800

    submitA1

--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -6,14 +6,77 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class ViewController: UIViewController {
+//Step2.1: add ", UITableViewDataSource, UITableViewDelegate"
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    var movies = [[String:Any]]()   //Step1.1: declare array of dictionary
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        print("Hello")
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) { (data, response, error) in
+             // This will run when the network request returns
+             if let error = error {
+                    print(error.localizedDescription)
+             } else if let data = data {
+                    let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+
+                 //Setp1.2: access the data from api/json and assign to dictionary and casting
+                 self.movies = dataDictionary["results"] as! [[String:Any]]
+                 
+                 //Step2.3: reload data again and again !important
+                 self.tableView.reloadData()
+                 
+                 print(dataDictionary)
+                 
+                    // TODO: Get the array of movies
+                    // TODO: Store the movies in a property to use elsewhere
+                    // TODO: Reload your table view data
+
+             }
+        }
+        task.resume()
     }
 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count;
+    }
+    
+    //Step2.2: implement functions
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell") as! MovieCell
+    
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        let synopsis = movie["overview"] as! String
+        
+        cell.titleLabel.text = title
+        cell.synopsisLabel.text = synopsis
+        
+        //get the url from API
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string:baseUrl + posterPath)
+        
+        cell.posterView.af.setImage(withURL: posterUrl!)
+        
+        return cell
+    }
+    
+    
 
 }
 
---
+++ README.md
@@ -0,0 +1,27 @@
+# Flix
+
+Flix is an app that allows users to browse movies from the [The Movie Database API](http://docs.themoviedb.apiary.io/#).
+
+---
+
+## Flix Part 1
+
+### User Stories
+
+#### REQUIRED (10pts)
+- [x] (2pts) User sees an app icon on the home screen and a styled launch screen.
+- [x] (5pts) User can view and scroll through a list of movies now playing in theaters.
+- [x] (3pts) User can view the movie poster image for each movie.
+
+#### BONUS
+- [ ] (2pt) User can view the app on various device sizes and orientations.
+- [x] (1pt) Run your app on a real device.
+
+### App Walkthrough GIF
+`TODO://` Add the URL to your animated app walkthough `gif` in the image tag below, `YOUR_GIF_URL_HERE`. Make sure the gif actually renders and animates when viewing this README. (🚫 Remove this paragraph after after adding gif)
+
+
+<img src="https://i.imgur.com/V2gekwm.gif" width=250><br>
+
+### Notes
+It's comfortable to follow with the instructor step by step. The only challange is that the position of some icons of Xcode that shown in vedios is different from mine. It's taken me some time to find them.
